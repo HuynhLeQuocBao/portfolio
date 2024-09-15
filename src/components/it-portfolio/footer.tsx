9dcf556878ed777c2013ce6f11edf8d5500cad3a
Bill <bill.h@itcgroup.io>
2024-09-15 19:40:11 +0700

    Fix

--- src/components/it-portfolio/footer.tsx
+++ src/components/it-portfolio/footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="w-full h-20 flex flex-col justify-center bg-gray-600">
       <div className="flex flex-row justify-center">
-        <p className="text-white text-lg leading-6 font-poppins font-normal tracking-wide">
-          © 2024 Footer
+        <p className="text-white text-lg leading-6 font-poppins font-normal tracking-wide p-2 text-center">
+          © 2024 Footer - Author: Do Khanh Huyen, Ly Duy Nam, Huynh Le Quoc Bao, Mai Hong Phuc.
         </p>
       </div>
     </div>
